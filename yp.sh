7ccd22c32e4b5465fd2f75c026dc9b6249ac991d
Medik2007 <meeedik2007@gmail.com>
2023-09-24 09:40:45 +0300

    run

--- yp.sh
+++ yp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qutebrowser --target window ":open http://127.0.0.1:8000" ":open -t https://gpt-chatbot.ru/chatgpt-3-5-besplatno-i-bez-registracii" & sleep 1 && bspc node -d ^2 &
+firefox http://127.0.0.1:8000 https://gpt-chatbot.ru/chatgpt-3-5-besplatno-i-bez-registracii & sleep 1 && bspc node -d ^2 &
 alacritty -e /bin/bash -c "bspc node -d ^3 &&
                            cd ~/prj/youngplanet-env &&
                            source bin/activate &&
