bb2ec5a2be2e92bd72479153b0de873ffca0f0cd
Medik2007 <meeedik2007@gmail.com>
2023-10-13 13:04:52 +0300

    run

--- yp.sh
+++ yp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-firefox http://127.0.0.1:8000 https://chat.geekgpt.org/& sleep 1 && bspc node -d ^2 &
-alacritty -e /bin/bash -c "bspc node -d ^3 &&
+firefox http://127.0.0.1:8000 https://chat.geekgpt.org/& sleep 1 && bspc node -d ^7 &
+alacritty -e /bin/bash -c "bspc node -d ^8 &&
                            cd ~/prj/youngplanet-env &&
                            source bin/activate &&
                            cd youngplanet &&
