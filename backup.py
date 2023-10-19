c02470212856b609ba32a37336fe586aa57ace55
Medik2007 <meeedik2007@gmail.com>
2023-10-19 21:06:59 +0300

    Scripts

--- backup.py
+++ backup.py
@@ -8,25 +8,32 @@ def run(name, event):
     sys.stdout.write(f'{name}: backup...')
     sys.stdout.flush()
     while not event.is_set():
-        for _ in range(3):
-            time.sleep(0.5)
-            sys.stdout.write('\033[D \033[D')
-            sys.stdout.flush()
+        sys.stdout.write(f'\r{name}: backup..')
+        sys.stdout.flush()
         time.sleep(0.5)
-        sys.stdout.write('...')
+        sys.stdout.write(f'\r{name}: backup.')
+        sys.stdout.flush()
+        time.sleep(0.5)
+        sys.stdout.write(f'\r{name}: backup')
+        sys.stdout.flush()
+        time.sleep(0.5)
+        sys.stdout.write(f'\r{name}: backup...')
         sys.stdout.flush()
     sys.stdout.write(f'\r{name}: backup completed')
     sys.stdout.flush()
 
-
-def push(path, name, commit=None):
+def push(path, name, commit=None, dirs=None):
     try:
         repo = Repo(HOME + path)
         if repo.is_dirty():
             event = threading.Event()
             threading.Thread(target=run, args=(name, event)).start()
             if commit == None: commit = name
-            repo.git.add(update=True)
+            if dirs == None:
+                repo.git.add(update=True)
+            else:
+                for i in dirs:
+                    repo.git.add()
             repo.index.commit(commit)
             repo.git.branch("--set-upstream-to=origin/master", "master")
             origin = repo.remote(name='origin')
@@ -55,14 +62,8 @@ def notes():
     push('nts/', 'Notes')
 
 def projects(name, commit):
-    if name:
-        os.chdir(os.path.expanduser(f'~/prj/{name}'))
-        subprocess.call(['git', 'add', '.'])
-        subprocess.call(['git', 'commit', '-m', commit])
-        subprocess.call(['git', 'push', 'origin', 'master'])
-        print(f'{name} project backup completed')
-    else:
-        print('Enter a valid project name')
+    if name: push(f'prj/{name}', name, commit)
+    else: print('Enter a valid project name')
 
 parser = argparse.ArgumentParser(description='System backup script')
 parser.add_argument('-t', default='system', help='Specify what to backup')
